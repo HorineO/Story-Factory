5f21d2694b5514ce101d38ff8abc8d75893dd46f
ohrol <ohrol@qq.com>
2025-06-25 19:15:26 +0800

    new feature: Add NewNodeTab component with styling

--- frontend/src/components/tabs/NewNodeTab.js
+++ frontend/src/components/tabs/NewNodeTab.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './NewNodeTab.css';
 
 const NewNodeTab = ({ onDragStart }) => {
     return (
         <div>
             <div className="dndnode" onDragStart={(event) => onDragStart(event, 'default')} draggable>
-                Default Node
+                默认节点
+            </div>
+            <div className="dndnode" onDragStart={(event) => onDragStart(event, 'input')} draggable>
+                输入节点
+            </div>
+            <div className="dndnode" onDragStart={(event) => onDragStart(event, 'default')} draggable>
+                生成节点
+            </div>
+            <div className="dndnode" onDragStart={(event) => onDragStart(event, 'output')} draggable>
+                输出节点
             </div>
             {/* 可以添加更多类型的节点 */}
         </div>
